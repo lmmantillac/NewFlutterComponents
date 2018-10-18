0ba603d02e05a2729112cfb8fa6b05246e3d77b9
luis_mantilla <lmmantilla9350@gmail.com>
2018-10-18 16:54:58 -0400

    Add StateFull Widget

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,8 @@ import './ui/home2.dart';
 void main(){
   runApp(new MaterialApp(
     title: "Simple Layouts",
-    home: new Home2(),
+    //home: new Home2(),
+    home: HelloYou(),
   ));
 }
 
@@ -31,3 +32,42 @@ class HelloFlutterApp extends StatelessWidget{
 //widgets are objects, and objects are instances of clases
 
 */
+
+class HelloYou extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState()=>_HelloYouState();
+
+}
+///_ is used to declarate a private class
+class _HelloYouState extends State<HelloYou>{
+  String name = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Helo"),
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(15.5),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(
+                hintText: "Please insert your name"
+              ),
+              onChanged: (String string){
+                setState(() {
+                  name = string;
+                });
+              },
+            ),
+            Text("Hello "+ name + "!"),
+          ],
+        ),
+      ),
+    );
+  }
+
+}
